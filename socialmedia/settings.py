7d83acb7cf1692d366f53823a4cff59530f1dd2e
unknown <kollarevanth@gmail.com>
2018-07-26 21:29:59 +0530

    make it better

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -166,4 +166,3 @@ MEDIA_URL='/twitterreact/public/image/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-django_heroku.settings(locals())
