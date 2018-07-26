4d6550133b9286d5eebead4d6a3854a356d60def
unknown <kollarevanth@gmail.com>
2018-07-26 20:33:58 +0530

    make it better

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -109,7 +109,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'socialmedia',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'USER':"root",
         'PASSWORD': ""
     }
